9aeb423a9bd08a273fb1ea459a667f387d169ef2
70k11 <wielgus.piotr@gmail.com>
2019-03-23 15:19:42 +0100

    hackathon

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    UIManager currentUIManager;
+    public void Init(UIManager uIManager)
+    {
+        currentUIManager = uIManager;
+    }
+
     public void CheckIfKilled(Vector3 explosionPosition, float range)
     {
         float dist = (transform.position - explosionPosition).magnitude;
         if (dist > range)
         {
-            //0
+            //GetComponent<Renderer>().material.color = Color.yellow;
+
         }
         else
         {
             float force = 1.0f - dist / range;
-            if(force > 0)
+            if(force > 0.5)
             {
-                GetComponent<Renderer>().material.color = Color.black;
+                //GetComponent<Renderer>().material.color = Color.black;
+                Destroy(gameObject);
             }
         }
     }
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject ObjectPrefab;
+    public float SpawnTimer = 10f;
+    public UIManager CurrentUIManager;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnObject", 0f, SpawnTimer);
+    }
+
+    void SpawnObject()
+    {
+        float objectXPosition = Random.Range(-48, 48);
+        float objectZPosition = Random.Range(-7, 45);
+
+        Vector3 calculatedPosition = new Vector3(objectXPosition, transform.position.y, objectZPosition);
+
+
+        GameObject spawnObject = Instantiate(ObjectPrefab, calculatedPosition, Quaternion.identity);
+        spawnObject.GetComponent<Enemy>()?.Init(CurrentUIManager);
+    }
+}
